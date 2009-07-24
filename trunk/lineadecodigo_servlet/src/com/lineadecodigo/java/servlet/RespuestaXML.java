7733b29f29d0a27b5064be7fd9ed9353dc30c414
vcuervo <vcuervo@c27369aa-5e45-0410-a428-534aef879a8c>
2009-07-24 23:49:27 +0000

    Servlet que devuelve XML

--- trunk/lineadecodigo_servlet/src/com/lineadecodigo/java/servlet/RespuestaXML.java
+++ trunk/lineadecodigo_servlet/src/com/lineadecodigo/java/servlet/RespuestaXML.java
@@ -1,36 +1,36 @@
 package com.lineadecodigo.java.servlet;
 
+/**
+ * @file RespuestaXML.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   25.julio.2009
+ * @url  http://lineadecodigo.com/java/servlet-que-devuelve-xml/
+ * @description Servlet que devuelve una respuesta XML
+ */
+
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RespuestaXML
- */
 public class RespuestaXML extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public RespuestaXML() {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		Response.setContentType("text/xml");
+		PrintWriter out = response.getWriter();
+		out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><nombre>Linea de Codigo</nombre>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request,response);
 	}
 
 }
