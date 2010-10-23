f4203f2cb7996efea7a53c5f25741e93c314e11e
vcuervo <vcuervo@c27369aa-5e45-0410-a428-534aef879a8c>
2010-10-23 00:29:18 +0000

    Vocales de una palabra

---
+++ lineadecodigo_java/src/com/lineadecodigo/java/string/VocalesPalabra.java
@@ -0,0 +1,32 @@
+package com.lineadecodigo.java.string;
+
+/**
+ * @file VocalesPalabra.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   23/octubre/2010
+ * @url    http://lineadecodigo.com/java/contar-las-vocales-de-una-palabra-con-java/
+ * @description Código que cuenta las letras de una palabra 
+ */
+
+public class VocalesPalabra {
+
+	public static void main(String[] args) {
+
+		//String sPalabra = "Supercalifragilisticoespialidoso";
+		String sPalabra ="murcielago";
+		
+		int contador = 0;
+		
+		for(int x=0;x<sPalabra.length();x++) {			
+			if ((sPalabra.charAt(x)=='a') || (sPalabra.charAt(x)=='e') || (sPalabra.charAt(x)=='i') || (sPalabra.charAt(x)=='o') || (sPalabra.charAt(x)=='u')){
+				contador++;
+			}
+		}
+		
+		System.out.println("La palabra " + sPalabra + "contiene " + contador + " vocales");
+		
+
+	}
+
+}
