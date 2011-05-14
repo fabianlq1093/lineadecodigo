5306e1e0bd15cd1f8fc607b530122ec3578a7581
vcuervo <vcuervo@c27369aa-5e45-0410-a428-534aef879a8c>
2011-05-14 18:41:55 +0000

    Dígitos de un número

---
+++ trunk/lineadecodigo_java/src/com/lineadecodigo/java/basico/NumeroDigitos.java
@@ -0,0 +1,20 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file NumeroCapicua.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date  14-mayo-2011
+ * @url    http://lineadecodigo.com/java/digitos-de-un-numero/
+ * @description Código que nos dice cuántos dígitos tiene un número 
+ */
+
+public class NumeroDigitos {
+
+	public static void main(String[] args) {			
+		int iNumero = 123458;
+		String x = Integer.toString(iNumero);
+		System.out.println(iNumero + " tiene " + x.length() + " dígitos");		
+	}
+	
+}
