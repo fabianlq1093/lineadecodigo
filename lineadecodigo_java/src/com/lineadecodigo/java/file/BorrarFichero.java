0688907506f60bc7adc3492abef067afb22d9fca
vcuervo <vcuervo@gmail.com>
2011-09-14 22:49:38 +0000

    Correción de URL

--- lineadecodigo_java/src/com/lineadecodigo/java/file/BorrarFichero.java
+++ lineadecodigo_java/src/com/lineadecodigo/java/file/BorrarFichero.java
@@ -3,11 +3,11 @@ package com.lineadecodigo.java.file;
 import java.io.File;
 
 /**
- * @file LeerFichero.java
- * @version 1.0
+ * @file BorrarFichero.java
+ * @version 1.1
  * @author Linea de Codigo (http://lineadecodigo.com)
- * @date   26-febrero-08
- * @url    http://lineadecodigo.com/
+ * @date   26/febrero/08
+ * @url    http://lineadecodigo.com/java/borrar-un-fichero-con-java/
  * @description Borrar un fichero desde un programa Java
  */
 
@@ -16,7 +16,7 @@ public class BorrarFichero {
 
 	public static void main(String[] args) {
 		
-		String sFichero = "fichero.txt";
+		String sFichero = "c:\\prueba\\prueba.txt";
 		File f = new File(sFichero);
 		
 		if (f.delete())
