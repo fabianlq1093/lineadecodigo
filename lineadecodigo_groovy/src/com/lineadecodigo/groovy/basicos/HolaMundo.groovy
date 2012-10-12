6981f83725e15daee7470e0ca0d171061b5a405e
vcuervo <vcuervo@gmail.com>
2012-10-12 22:36:50 +0000

    Actualizados los comentarios

--- lineadecodigo_groovy/src/com/lineadecodigo/groovy/basicos/HolaMundo.groovy
+++ lineadecodigo_groovy/src/com/lineadecodigo/groovy/basicos/HolaMundo.groovy
@@ -1,3 +1,12 @@
 package com.lineadecodigo.groovy.basicos
 
+/**
+* @file HolaMundo.groovy
+* @version 1.0
+* @author Victor Cuervo (http://lineadecodigo.com)
+* @date   12-octubre-2012
+* @url    http://lineadecodigo.com/groovy/hola-mundo-en-groovy/
+* @description Programa Hola Mundo en Groovy
+*/
+
 println("Hola Mundo")
