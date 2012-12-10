aa66fd87f6ff7bf3733e72f92b742bbfefc24ade
vcuervo@gmail.com <vcuervo@gmail.com@c27369aa-5e45-0410-a428-534aef879a8c>
2012-12-10 07:42:47 +0000

    Listar una lista en Groovy

---
+++ trunk/lineadecodigo_groovy/src/com/lineadecodigo/groovy/listas/ListarLista.groovy
@@ -0,0 +1,20 @@
+package com.lineadecodigo.groovy.listas
+
+/**
+ * @file HolaMundo.groovy
+ * @version 1.0
+ * @author Victor Cuervo (http://lineadecodigo.com)
+ * @date   10-diciembre-2012
+ * @url    http://lineadecodigo.com/groovy/crear-una-lista-en-groovy/
+ * @description Programa que crea una lista en Groovy y la recorre
+ */
+
+// Definir y recorrer una lista
+def lista = ["Victor","Julio","Mar’a","Ignacio","Virginia"]
+
+print "Imprimimos el contenido de la lista"
+for (elemento in lista)
+  println elemento
+
+
+
