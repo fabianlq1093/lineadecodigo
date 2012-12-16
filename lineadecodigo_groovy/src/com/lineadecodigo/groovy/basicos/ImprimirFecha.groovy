0d8b2471769b9f3680c75538531d96e8917181b9
vcuervo <vcuervo@gmail.com>
2012-12-16 13:04:01 +0000

    Obtener la fecha de hoy con Groovy

---
+++ lineadecodigo_groovy/src/com/lineadecodigo/groovy/basicos/ImprimirFecha.groovy
@@ -0,0 +1,13 @@
+package com.lineadecodigo.groovy.basicos
+
+/**
+ * @file HolaMundo.groovy
+ * @version 1.0
+ * @author Victor Cuervo (http://lineadecodigo.com)
+ * @date   14-diciembre-2012
+ * @url    http://lineadecodigo.com/groovy/obtener-la-fecha-de-hoy-con-groovy/
+ * @description Programa que muestra la fecha actual por consola
+ */
+
+hoy = new Date()
+println hoy
