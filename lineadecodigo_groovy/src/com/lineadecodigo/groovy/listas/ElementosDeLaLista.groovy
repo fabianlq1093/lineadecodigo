09221406c959666988570ad8dc967c0d30d162c9
vcuervo <vcuervo@gmail.com>
2012-12-26 00:32:57 +0000

    Número de elementos de una lista en Groovy

---
+++ lineadecodigo_groovy/src/com/lineadecodigo/groovy/listas/ElementosDeLaLista.groovy
@@ -0,0 +1,16 @@
+package com.lineadecodigo.groovy.listas
+
+/**
+ * @file ElementosDeLaLista.groovy
+ * @version 1.0
+ * @author Victor Cuervo (http://lineadecodigo.com)
+ * @date   14-diciembre-2012
+ * @url    http://lineadecodigo.com/groovy/tamano-de-una-lista-en-groovy/
+ * @description Programa que calcula el nœmero de elemento de una lista
+ */
+
+// Definir y recorrer una lista
+def lista = ["Victor","Julio","Mar’a","Ignacio","Virginia"]
+
+println lista.size
+println "El nœmero de elementos de la lista es $lista.size"
