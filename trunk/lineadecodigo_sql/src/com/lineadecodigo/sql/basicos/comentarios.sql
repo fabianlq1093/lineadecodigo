b96988b2e76dddaa66b54223256ac8445e3dd11d
vcuervo@gmail.com <vcuervo@gmail.com@c27369aa-5e45-0410-a428-534aef879a8c>
2013-01-29 00:11:06 +0000

    Comentarios en SQL.

---
+++ trunk/lineadecodigo_sql/src/com/lineadecodigo/sql/basicos/comentarios.sql
@@ -0,0 +1,31 @@
+/**
+ * @file comentarios.sql
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   29-enero-2013
+ * @url		http://lineadecodigo.com/sql/comentarios-en-sql/
+ * @description C—mo poner comentarios en SQLe
+ */
+
+
+-- Comentario en una l’nea para SQL. Del ANSI/SQL
+
+
+/*
+ * Comentario en varias l’neas.
+ * Compatible con el est‡ndar SQL99
+ */
+
+select *
+from libros
+--where idautor = 1
+
+/*
+
+Comentarios espec’ficos de otros SGBD
+
+{Comentario con llaves}
+# Esto es un comentario
+REM Esto es un comentario
+
+*/
