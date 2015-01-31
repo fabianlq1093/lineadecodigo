f5a7301a6e8d714f1cb30be93135d562ff3add4e
vcuervo@gmail.com <vcuervo@gmail.com@c27369aa-5e45-0410-a428-534aef879a8c>
2015-01-31 19:22:09 +0000

    Desbordar una variable

--- trunk/lineadecodigo_java/src/com/lineadecodigo/java/basico/variables/VariablesTransitorias.java
+++ trunk/lineadecodigo_java/src/com/lineadecodigo/java/basico/variables/VariablesTransitorias.java
@@ -1,12 +1,21 @@
 package com.lineadecodigo.java.basico.variables;
 
+/**
+ * @file VariablesTransitorias.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   31/enero/2015
+ * @url		http://lineadecodigo.com/java/variables-java-transient/
+ * @description Explicaci—n sobre el funcionamiento del argumento transient
+ */
+
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Date;
 import com.lineadecodigo.base.Usuario;
-
 
 public class VariablesTransitorias {
 	
